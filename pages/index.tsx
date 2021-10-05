5637902337da3f09e11d3799b6015e3751dd3fd1
Bryan Araujo <bryanmaraujo544@gmail.com>
2021-10-05 10:38:40 -0300

    feat: finishing header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next';
-import { Heading, HeadingProps, Button, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Logo } from '../components/Logo';
+import { Heading, HeadingProps, Button, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
+import { Header } from '../components/Header';
 
 // Chakra component with motion element
 export const MotionHeading = motion<HeadingProps>(Heading);
@@ -13,17 +15,7 @@ const Home: NextPage = () => {
      const color = useColorModeValue("text.light", "text.dark");
      return (
           <>
-               <MotionHeading
-                    whileHover={{ fontSize: '12px' }}
-                    color={color}
-               >
-                    Hello World!
-               </MotionHeading>
-               <Button 
-                    onClick={() => toggleColorMode()}
-               >
-                    Clich Here to Change Color Moder
-               </Button>
+               <Header />
           </>
      )
 }
