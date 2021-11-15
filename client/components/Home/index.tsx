3e73b99ec540ff29a20c32bc13ad18848d2a64e7
Bryan Araujo <bryanmaraujo544@gmail.com>
2021-11-15 20:46:59 -0300

    fix: creating the authContext and moving the dign logic to it

--- client/components/Home/index.tsx
+++ client/components/Home/index.tsx
@@ -6,54 +6,47 @@ import { Box, Flex } from '@chakra-ui/react';
 import { useDarkLightColors } from '../../hooks/useDarkLightColors';
 
 type User = {
-  avatar_url: string,
-  bio: string,
-  followers: number,
-  following: number,
-  name: string,
-  login: string,
-  public_repos: number,
-  starred_repos: number,
-  mostUsedLangs: string[],
-  followers_url: string,
-  following_url: string,
-  repos_url: string,
-  starred_url: string
+  avatar_url: string;
+  bio: string;
+  followers: number;
+  following: number;
+  name: string;
+  login: string;
+  public_repos: number;
+  starred_repos: number;
+  mostUsedLangs: string[];
+  followers_url: string;
+  following_url: string;
+  repos_url: string;
+  starred_url: string;
+};
 
-}
-
-export const Home = () => {
+export const Home = ({ token }: Record<string, string>) => {
   const bgColor = useDarkLightColors('bg.light', 'bg.dark');
   const [user, setUser] = useState<null | User>(null);
   const [hasError, setHasError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  console.log({user});
 
   return (
-    <Box 
+    <Box
       minHeight="100vh"
-      maxWidth="100vw" 
-      overflow-x="hidden" 
-      bg={bgColor} 
+      maxWidth="100vw"
+      overflow-x="hidden"
+      bg={bgColor}
       px={[8, 12, 16, 20, 24]}
     >
       <Header />
       <Flex direction="column" mt={[12, 16, 20, null, 24]}>
+        {/* Search Bar */}
+        <SearchBar
+          setUser={setUser}
+          setHasError={setHasError}
+          setIsLoading={setIsLoading}
+        />
 
-          {/* Search Bar */}
-          <SearchBar
-            setUser={setUser}
-            setHasError={setHasError}
-            setIsLoading={setIsLoading}
-          />
-
-          {/* User Box */}
-          <UserBox 
-            isLoading={isLoading}
-            hasError={hasError}
-            user={user}
-          />
+        {/* User Box */}
+        <UserBox isLoading={isLoading} hasError={hasError} user={user} />
       </Flex>
     </Box>
-  )
-}
+  );
+};
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,26 @@
 import type { NextPage } from 'next';
+import { ReactNode } from 'react';
 import { GetServerSideProps } from 'next';
 import { Home as HomeComp } from '../components/Home';
 import nookies from 'nookies';
 
-const Home: NextPage = () => {
-  return <HomeComp />;
+import { serverApi } from '../services/serverApi';
+
+type propTypes = {
+  children: ReactNode;
+  token: string;
+};
+
+const Home = (props: propTypes) => {
+  return <HomeComp token={props.token} />;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const cookies = nookies.get(context);
-  console.log({ cookies });
+  const token = cookies['@token'];
   return {
     props: {
-      oi: 'oi',
+      token,
     },
   };
 };
