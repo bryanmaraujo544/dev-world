625b87ec23805de95a43d674ece75d66fe2d19b0
Bryan Araujo <bryanmaraujo544@gmail.com>
2021-11-17 20:42:36 -0300

    feat: setting an expires time to jwt

--- server/src/app/utils/createToken.js
+++ server/src/app/utils/createToken.js
@@ -1,7 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function createToken({ name, github_username }) {
-  const token = jwt.sign({ name, githubUsername: github_username }, process.env.JWT_SECRET);
+  const token = jwt.sign(
+    { name, githubUsername: github_username }, 
+    process.env.JWT_SECRET,
+    { expiresIn: '72h' }
+  );
   return token;
 }
 
